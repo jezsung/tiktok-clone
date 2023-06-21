b92853ebea58e08833a7eebfa257c9ff930a158f
Jaesung Hong <jaesunghong@Jaesungs-Mac-mini.local>
2023-06-21 21:01:10 +0900

    feat: add flashcard back

--- components/Flashcard.tsx
+++ components/Flashcard.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from 'react-native';
+import { useRef, useState } from 'react';
+import { Animated, Easing, Text, View } from 'react-native';
 
 import ArrowIcon from '../assets/icons/arrow.svg';
 import VideoIcon from '../assets/icons/video.svg';
@@ -25,6 +26,21 @@ const Flashcard: React.FC<{
   tag,
   user: { name, avatar },
 }) => {
+  const [side, setSide] = useState<'front' | 'back'>('front');
+  const opacity = useRef(new Animated.Value(1)).current;
+
+  function flip() {
+    setSide((prev) => (prev === 'front' ? 'back' : 'front'));
+    opacity.stopAnimation((value) => {
+      Animated.timing(opacity, {
+        toValue: value > 0.5 ? 0 : 1,
+        duration: 300,
+        easing: Easing.out(Easing.ease),
+        useNativeDriver: true,
+      }).start();
+    });
+  }
+
   return (
     <View style={{ flex: 1, flexDirection: 'column', gap: 16 }}>
       <View
@@ -38,62 +54,37 @@ const Flashcard: React.FC<{
           paddingLeft: 16,
         }}
       >
-        <View
-          style={{
-            flex: 1,
-            flexDirection: 'column',
-            gap: 24,
-          }}
-        >
-          <View
+        {side === 'front' && (
+          <Animated.View onTouchEnd={flip} style={{ opacity }}>
+            <Front
+              front={flashcardFront}
+              description={description}
+              tag={tag}
+              username={name}
+            />
+          </Animated.View>
+        )}
+        {side === 'back' && (
+          <Animated.View
+            onTouchEnd={flip}
             style={{
-              flex: 1,
-              flexDirection: 'column',
-              justifyContent: 'center',
-              width: 286,
+              opacity: opacity.interpolate({
+                inputRange: [0, 1],
+                outputRange: [1, 0],
+              }),
             }}
           >
-            <Text
-              style={{
-                color: 'white',
-                fontFamily: 'sf-pro-rounded-400',
-                fontSize: 21,
-                lineHeight: 25.06,
-              }}
-            >
-              {flashcardFront}
-            </Text>
-          </View>
-          <View
-            style={{
-              flexDirection: 'column',
-              gap: 6,
-            }}
-          >
-            <Text
-              style={{
-                color: 'white',
-                fontFamily: 'sf-pro-rounded-600',
-                fontSize: 15,
-                lineHeight: 17.9,
-              }}
-            >
-              {name}
-            </Text>
-            <Text
-              style={{
-                color: 'white',
-                fontFamily: 'sf-pro-rounded-400',
-                fontSize: 12,
-                lineHeight: 14.32,
-              }}
-            >
-              {description}{' '}
-              <Text style={{ fontFamily: 'sf-pro-rounded-700' }}>{tag}</Text>
-            </Text>
-          </View>
-        </View>
-        <ActionBar avatar={avatar} onFlip={() => {}} />
+            <Back
+              front={flashcardFront}
+              back={flashcardBack}
+              description={description}
+              tag={tag}
+              username={name}
+            />
+          </Animated.View>
+        )}
+
+        <ActionBar avatar={avatar} onFlip={flip} />
       </View>
       <View
         style={{
@@ -119,6 +110,250 @@ const Flashcard: React.FC<{
           </Text>
         </View>
         <ArrowIcon width={11} height={16} fill="white" />
+      </View>
+    </View>
+  );
+};
+
+const Front: React.FC<{
+  front: string;
+  description: string;
+  tag: string;
+  username: string;
+}> = ({ front, description, tag, username }) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+        gap: 24,
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          width: 286,
+        }}
+      >
+        <Text
+          style={{
+            color: 'white',
+            fontFamily: 'sf-pro-rounded-400',
+            fontSize: 21,
+            lineHeight: 25.06,
+          }}
+        >
+          {front}
+        </Text>
+      </View>
+      <View
+        style={{
+          flexDirection: 'column',
+          gap: 6,
+        }}
+      >
+        <Text
+          style={{
+            color: 'white',
+            fontFamily: 'sf-pro-rounded-600',
+            fontSize: 15,
+            lineHeight: 17.9,
+          }}
+        >
+          {username}
+        </Text>
+        <Text
+          style={{
+            color: 'white',
+            fontFamily: 'sf-pro-rounded-400',
+            fontSize: 12,
+            lineHeight: 14.32,
+          }}
+        >
+          {description}{' '}
+          <Text style={{ fontFamily: 'sf-pro-rounded-700' }}>{tag}</Text>
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const Back: React.FC<{
+  front: string;
+  back: string;
+  description: string;
+  tag: string;
+  username: string;
+}> = ({ front, back, description, tag, username }) => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+        gap: 24,
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'center',
+          gap: 24,
+          width: 286,
+        }}
+      >
+        <Text
+          style={{
+            color: 'white',
+            fontFamily: 'sf-pro-rounded-400',
+            fontSize: 21,
+            lineHeight: 25.06,
+          }}
+        >
+          {front}
+        </Text>
+
+        <View
+          style={{
+            alignSelf: 'stretch',
+            backgroundColor: 'white',
+            opacity: 0.2,
+            height: 2,
+          }}
+        />
+
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
+          <View
+            style={{
+              flexDirection: 'column',
+              gap: 4,
+            }}
+          >
+            <Text
+              style={{
+                color: '#2DC59F',
+                fontFamily: 'sf-pro-rounded-800',
+                fontSize: 13,
+                lineHeight: 15.51,
+                paddingBottom: 4,
+              }}
+            >
+              Answer
+            </Text>
+            <Text
+              style={{
+                color: 'white',
+                opacity: 0.7,
+                fontFamily: 'sf-pro-rounded-400',
+                fontSize: 21,
+                lineHeight: 25.06,
+              }}
+            >
+              {back}
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'column',
+              gap: 5,
+            }}
+          >
+            <Text
+              style={{
+                color: 'white',
+                opacity: 0.6,
+                fontFamily: 'sf-pro-rounded-400',
+                fontSize: 15,
+                lineHeight: 17.9,
+              }}
+            >
+              How well did you know this?
+            </Text>
+            <View style={{ flexDirection: 'row', gap: 8 }}>
+              {[1, 2, 3, 4, 5].map((rating) => {
+                const color = (() => {
+                  switch (rating) {
+                    case 1:
+                      return '#F17D23';
+                    case 2:
+                      return '#FBB668';
+                    case 3:
+                      return '#FFD449';
+                    case 4:
+                      return '#16624F';
+                    case 5:
+                      return '#1F8A70';
+                    default:
+                      throw new Error('Unimplemented rating color');
+                  }
+                })();
+
+                return (
+                  <View
+                    key={rating}
+                    style={{
+                      flex: 1,
+                      flexDirection: 'column',
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      height: 52,
+                      backgroundColor: color,
+                      borderRadius: 8,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        color: 'white',
+                        fontFamily: 'sf-pro-rounded-600',
+                        fontSize: 17,
+                        lineHeight: 20.29,
+                      }}
+                    >
+                      {rating}
+                    </Text>
+                  </View>
+                );
+              })}
+            </View>
+          </View>
+        </View>
+      </View>
+
+      <View
+        style={{
+          flexDirection: 'column',
+          gap: 6,
+        }}
+      >
+        <Text
+          style={{
+            color: 'white',
+            fontFamily: 'sf-pro-rounded-600',
+            fontSize: 15,
+            lineHeight: 17.9,
+          }}
+        >
+          {username}
+        </Text>
+        <Text
+          style={{
+            color: 'white',
+            fontFamily: 'sf-pro-rounded-400',
+            fontSize: 13,
+            lineHeight: 14.32,
+          }}
+        >
+          {description}{' '}
+          <Text style={{ fontFamily: 'sf-pro-rounded-700' }}>{tag}</Text>
+        </Text>
       </View>
     </View>
   );
