88422650c5c9894d64b73122cc2204f07c496b5b
Jaesung Hong <jaesunghong@Jaesungs-MacBook-Pro.local>
2023-06-21 17:27:02 +0900

    chore: remove type checking from lint-staged.config.js

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  '**/*.{ts,tsx}': 'tsc-files --noEmit',
   '**/*.{json,js,jsx,ts,tsx,md}': (filenames) =>
     filenames.map((filename) => `prettier --write ${filename}`),
   '**/*.{js,jsx,ts,tsx}': 'eslint --fix --max-warnings=0',
