98caf35872f3e6f06032f0e5013b52ca324a5a3b
Jaesung Hong <jaesunghong@Jaesungs-MacBook-Pro.local>
2023-06-21 16:15:50 +0900

    chore: add eslint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,4 +2,5 @@ module.exports = {
   '**/*.{ts,tsx}': 'tsc-files --noEmit',
   '**/*.{json,js,jsx,ts,tsx,md}': (filenames) =>
     filenames.map((filename) => `prettier --write ${filename}`),
+  '**/*.{js,jsx,ts,tsx}': 'eslint --fix',
 };
